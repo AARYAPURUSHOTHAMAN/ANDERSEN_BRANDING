6c83c7b1eb3ab09f67a8106275b71968b02c0993
Aarya Purushothaman <aarya.purushothaman@10xds.com>
2026-02-10 12:23:17 +0530

    changed api

--- services/getProspect.ts
+++ services/getProspect.ts
@@ -1,7 +1,7 @@
 
 import { GetProspectResult, VerificationResult } from '../types';
 
-const GETPROSPECT_API_KEY = '9d6a6998-bc25-4dda-aeee-dd9e8c7ec534';
+const GETPROSPECT_API_KEY = 'dd126647-ba99-4611-8390-0321b25d74c7';
 const BASE_URL = 'https://api.getprospect.com/public/v1/email';
 
 export async function findEmail(name: string, company: string): Promise<GetProspectResult> {
