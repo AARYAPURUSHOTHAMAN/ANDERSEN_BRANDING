843ea1ad57681a68511dea592996af033403cfa1
Aarya Purushothaman <aarya.purushothaman@10xds.com>
2026-03-13 16:17:54 +0530

    endpoint change

--- App.tsx
+++ App.tsx
@@ -1498,7 +1498,7 @@ const App: React.FC = () => {
         }));
 
         try {
-            const response = await fetch('https://3539-164-52-216-254.ngrok-free.app/enrich', {
+            const response = await fetch('https://bb23-164-52-216-254.ngrok-free.app/enrich', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -1603,7 +1603,7 @@ const App: React.FC = () => {
         }];
 
         try {
-            const response = await fetch('https://3539-164-52-216-254.ngrok-free.app/enrich', {
+            const response = await fetch('https://bb23-164-52-216-254.ngrok-free.app/enrich', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
